9ed5468ebd2fea58048a5ef90742db17bd8a4023
Shadrack <skmutai6@gmail.com>
2024-05-28 09:27:51 +0300

    lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -2,19 +2,24 @@ def create_an_empty_list():
     return []
 
 def create_a_list():
-    return ["April", "May", "June", "July"]
+    my_list = ["April", "May", "June", "July"]
+    return my_list
 
 def add_element_to_end_of_list(l, element):
-    return l.append(4, "August")
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return l.insert(0, "March")
+    l.insert(0, element)
+    return l
 
 def remove_element_from_end_of_list(l):
-    del l[-1]
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
     del l[0]
+    return ()
 
 def retrieve_first_element_from_list(l):
     return l[0]
